075b1ea182946ea80d8ba4fb6abe5573212ead30
Gocha <gochahome@gmail.com>
2022-01-02 10:52:12 +0300

    fix

--- test-capitalize/capitalize.test.js
+++ test-capitalize/capitalize.test.js
@@ -1,11 +1,15 @@
+import { strict as assert } from 'assert';
 import { capitalize } from './capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'jello');//более короткий и информативный вариант теста
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');//более короткий и информативный вариант теста
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
 
 console.log('Все тесты пройдены!');
+
